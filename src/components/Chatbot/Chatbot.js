b9393ab2f051fa94cf436dd8ae2ec9040e65e4ef
FullLothar <pablosalina1980@gmail.com>
2023-05-18 18:50:21 -0300

    Actualización de Chatbot.js

--- src/components/Chatbot/Chatbot.js
+++ src/components/Chatbot/Chatbot.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './Chatbot.css';
 import axios from 'axios';
 
+// Función para enviar el mensaje a la API de ChatGPT
+const sendMessageToChatGPT = async (message) => {
+  try {
+    const response = await axios.post('https://chat.openai.com/', {
+      message: message
+    });
+
+    // Procesar la respuesta de la API y mostrarla en el chatbot
+    const answer = response.data.answer;
+    console.log(answer);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 function Chatbot() {
   const handleSendMessage = () => {
     // Lógica para enviar el mensaje a la API de ChatGPT
+    const message = ""; // Obtén el mensaje del input
+    sendMessageToChatGPT(message);
   };
 
   return (
@@ -19,22 +36,6 @@ function Chatbot() {
       </div>
     </div>
   );
-
-  // Función para enviar el mensaje a la API de ChatGPT
-const sendMessageToChatGPT = async (message) => {
-  try {
-    const response = await axios.post('https://chat.openai.com/?model=text-davinci-002-render-sha', {
-      message: message
-    });
-
-    // Procesar la respuesta de la API y mostrarla en el chatbot
-    const answer = response.data.answer;
-    console.log(answer);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 }
 
 export default Chatbot;
